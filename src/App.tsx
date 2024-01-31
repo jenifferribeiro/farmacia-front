cffdc24e41fd23525d1fd80d6724c53abf4ccf7f
Jeniffer Ribeiro <js.ribei@hotmail.com>
2024-01-31 10:19:59 -0300

    Implementa CRUD-categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import FormCategoria from "./components/categoria/formcategoria/FormCategoria"
+import Footer from "./components/footer/Footer"
+import ListaCategorias from "./components/categoria/listacategorias/ListaCategorias"
+import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria"
 
 function App() {
+
   return (
     <>
-    <Navbar/>
-    <div className="min-h-[80vh]">
-    <Home/>
-    </div>
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-screen" >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormCategoria />} />
+            <Route path="/editarCategoria/:id" element={<FormCategoria />} />
+            <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-);
+  )
 }
-export default App;
+
+export default App
