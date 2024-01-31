c173e0e73de4d4f5a081d4ec5fea06766a95f165
Jeniffer Ribeiro <js.ribei@hotmail.com>
2024-01-31 10:29:30 -0300

    rotas-conclusao-do-projeto

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,27 @@
+import { Link } from "react-router-dom"
 
 
 
 function Navbar() {
- 
-  
 
-    return (
-      <>
-       <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-            <div className="container flex justify-between text-lg">
-              <div className='text-2xl font-bold uppercase'>Loja de games</div>
-  
-              <div className='flex gap-4'>
-                <div className='hover:underline'>Produtos</div>
-                <div className='hover:underline'>Categoria</div>
-                <div className='hover:underline'>Cadastrar Categoria</div>
-                <div className='hover:underline'>Perfil</div>
-                <div className='hover:underline'>Sair</div>
-              </div>
-            </div>
+
+
+  return (
+    <>
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>Loja de games</div>
+
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Produtos</Link>
+            <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastrarcategoria' className='hover:underline'>Cadastrar Categoria</Link>
+            <Link to='' className='hover:underline'>Sair</Link>
           </div>
-      </>
-    )
-  }
-  
-  export default Navbar
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
